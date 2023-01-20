f06ace6ec0119fe5168053ea002923c3ee4aaa93
watcharapon weeraborirak <69833839+watcharap0n@users.noreply.github.com>
2023-01-20 17:19:59 +0700

    Update Portfolio.vue

--- service_portfolio/client/components/Portfolio.vue
+++ service_portfolio/client/components/Portfolio.vue
@@ -38,7 +38,7 @@
           </strong>
 
           <div style="margin-top: 10px; margin-left: 10px">
-            <div style="color: grey">Between August 2022 to Present (4 month)</div>
+            <div style="color: grey">Between August 2022 to Present</div>
             <div style="color: grey">
               349 SJ Infinite 1 Business Complex, 28th Floor Vibhavadi Rangsit Rd, Chatuchak,
               Chatuchak, Bangkok 10900
